6d473c76fba7fa6633079beb9c4500873347b806
Adam Deschamp <1755763+apdesch@users.noreply.github.com>
2023-05-02 02:24:16 -0700

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
+# update packages
 npm i
 # rebuild build client and api server
 npm run build:api
 npm run build:client
 # restart api server
-pm2 restart dist/index.js
+pm2 restart server/dist/index.js
 # restart server
-systemctl restart nginx
-systemctl restart redis-server
+sudo systemctl restart nginx
+sudo systemctl restart redis-server
